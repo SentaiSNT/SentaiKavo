a8343a66bbd0a272ed3c3430aacce6e5208745c3
SentaiSNT <aldar.098123@gmail.com>
2025-07-20 20:29:37 +0300

    Update KavoUI.lua

--- KavoUI.lua
+++ KavoUI.lua
@@ -113,9 +113,9 @@ local themeStyles = {
     Midnight = {
         SchemeColor = Color3.fromRGB(161, 165, 162),
         Background = Color3.fromRGB(161, 165, 162),
-        Header = Color3.fromRGB(161, 165, 162),
+        Header = Color3.fromRGB(128, 187, 219),
         TextColor = Color3.fromRGB(255, 255, 255),
-        ElementColor = Color3.fromRGB(161, 165, 162)
+        ElementColor = Color3.fromRGB(128, 187, 219)
     },
     Sentinel = {
         SchemeColor = Color3.fromRGB(230, 35, 69),
